653549d29bf40be2898151e6ee97aa0cd397ebbc
shenlei162 <shenlei162@163.com>
2021-12-01 09:00:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,13 +25,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "Start",
+        play: "Music~",
+        bannar_coming: "Go!",
+        balloons_flying: "Something is Coming!",
+        cake_fadein: "Cake",
+        light_candle: "Light",
+        wish_message: "Happy Birthday!",
         story: "A MESSAGE FOR YOU",
     }
 };
